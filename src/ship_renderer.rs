e9e84ae122a2453d7b0fdd0824fa72b4dcddd5e2
felixnaredi <naredi@student.chalmers.se>
2021-08-31 20:56:26 +0200

    Some refactoring in ShipRenderer

--- src/ship_renderer.rs
+++ src/ship_renderer.rs
@@ -28,57 +28,54 @@ pub struct ShipRenderer
 
 impl ShipRenderer
 {
+    /// Creates a new `ShipRenderer`.
     pub fn new(
         context: &WebGlRenderingContext,
         descriptor: &ShipRendererDescriptor,
     ) -> Result<ShipRenderer, String>
     {
+        //
+        // Create program.
+        //
         let program = gl::link_program(
             context,
             &vertex_shader(context)?,
             &fragment_shader(context)?,
         )?;
 
+        //
+        // Initialize vertex buffer.
+        //
         let r = descriptor.wing_angle;
-        let vertices: [f32; 18] = [
-            // 0
-            1.0,
-            0.0,
-            0.0,
-            // 1
-            r.cos(),
-            r.sin(),
-            0.0,
-            // 2
-            descriptor.tail_x,
-            0.0,
-            0.0,
-            // 3
-            descriptor.tail_x,
-            0.0,
-            0.0,
-            // 4
-            r.cos(),
-            -r.sin(),
-            0.0,
-            // 5
-            1.0,
-            0.0,
-            0.0,
-        ];
-
-        let vertex_buffer = context.create_buffer().ok_or("failed to create buffer")?;
-        context.bind_buffer(WebGlRenderingContext::ARRAY_BUFFER, Some(&vertex_buffer));
-
-        unsafe {
-            let vertex_array = js_sys::Float32Array::view(&vertices);
-
-            context.buffer_data_with_array_buffer_view(
-                WebGlRenderingContext::ARRAY_BUFFER,
-                &vertex_array,
-                WebGlRenderingContext::STATIC_DRAW,
-            );
-        }
+        let vertex_buffer = gl::make_static_draw_array_buffer_f32(
+            context,
+            vec![
+                // 0
+                1.0,
+                0.0,
+                0.0,
+                // 1
+                r.cos(),
+                r.sin(),
+                0.0,
+                // 2
+                descriptor.tail_x,
+                0.0,
+                0.0,
+                // 3
+                descriptor.tail_x,
+                0.0,
+                0.0,
+                // 4
+                r.cos(),
+                -r.sin(),
+                0.0,
+                // 5
+                1.0,
+                0.0,
+                0.0,
+            ],
+        )?;
 
         Ok(ShipRenderer {
             program,
@@ -86,6 +83,7 @@ impl ShipRenderer
         })
     }
 
+    /// Renders `ship`.
     pub fn render(&self, context: &Context, ship: &Ship)
     {
         let gl = context.render_context();
@@ -99,38 +97,42 @@ impl ShipRenderer
         gl.use_program(Some(&self.program));
         gl.vertex_attrib_pointer_with_i32(0, 3, WebGlRenderingContext::FLOAT, false, 0, 0);
 
-        let model_matrix_location = gl.get_uniform_location(&self.program, "model_matrix");
-
+        //
+        // Calculate world matrix and set the uniform.
+        //
         let s = ship.size();
-        let scale = arr2(&[
-            [s[0], 0.0, 0.0, 0.0],
-            [0.0, s[1], 0.0, 0.0],
-            [0.0, 0.0, 1.0, 0.0],
-            [0.0, 0.0, 0.0, 1.0],
-        ]);
-
         let p = ship.position();
-        let transpose = arr2(&[
-            [1., 0., 0., 0.],
-            [0., 1., 0., 0.],
-            [0., 0., 1., 0.],
-            [p[0], p[1], 0., 1.],
-        ]);
-
         let r = ship.yaw();
-        let yaw = arr2(&[
+
+        let matrix = arr2(&[
             [r.cos(), r.sin(), 0., 0.],
             [-r.sin(), r.cos(), 0., 0.],
             [0., 0., 1., 0.],
             [0., 0., 0., 1.],
-        ]);
+        ])
+        .dot(&arr2(&[
+            [s[0], 0.0, 0.0, 0.0],
+            [0.0, s[1], 0.0, 0.0],
+            [0.0, 0.0, 1.0, 0.0],
+            [0.0, 0.0, 0.0, 1.0],
+        ]))
+        .dot(&arr2(&[
+            [1., 0., 0., 0.],
+            [0., 1., 0., 0.],
+            [0., 0., 1., 0.],
+            [p[0], p[1], 0., 1.],
+        ]));
 
+        let location = gl.get_uniform_location(&self.program, "world_matrix");
         gl.uniform_matrix4fv_with_f32_array(
-            model_matrix_location.as_ref(),
+            location.as_ref(),
             false,
-            (yaw.dot(&scale).dot(&transpose)).view().as_slice().unwrap(),
+            matrix.view().as_slice().unwrap(),
         );
 
+        //
+        // Set the perspective matrix uniform
+        //
         let location = gl.get_uniform_location(&self.program, "perspective_matrix");
         let matrix = arr2(context.foreground_perspective_matrix());
         gl.uniform_matrix4fv_with_f32_array(
@@ -139,6 +141,9 @@ impl ShipRenderer
             matrix.view().as_slice().unwrap(),
         );
 
+        //
+        // Draw ship.
+        //
         gl.draw_arrays(WebGlRenderingContext::TRIANGLES, 0, 6);
 
         gl.use_program(None);
@@ -152,11 +157,11 @@ fn vertex_shader(context: &WebGlRenderingContext) -> Result<WebGlShader, String>
         r#"
     attribute vec4 position;
 
-    uniform mat4 model_matrix;
+    uniform mat4 world_matrix;
     uniform mat4 perspective_matrix;
 
     void main() {
-      gl_Position = perspective_matrix * model_matrix * position;
+      gl_Position = perspective_matrix * world_matrix * position;
     }
     "#,
     )
@@ -167,8 +172,10 @@ fn fragment_shader(context: &WebGlRenderingContext) -> Result<WebGlShader, Strin
     gl::compile_fragment_shader(
         context,
         r#"
+    precision mediump float;
+
     void main() {
-        gl_FragColor = vec4(0.5, 0.7, 0.8, 1.0);
+        gl_FragColor = vec4(0.4, 0.7, 0.8, 1.0);
     }
     "#,
     )
